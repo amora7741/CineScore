be8cb93393f6a19cfd03f3f681634b0427aae112
amora7741 <mora7741@gmail.com>
2025-01-07 11:00:32 -0800

    Set up pagination routing

--- src/components/PageRouter.tsx
+++ src/components/PageRouter.tsx
@@ -1,21 +1,40 @@
 import {
   Pagination,
   PaginationContent,
+  PaginationEllipsis,
   PaginationItem,
   PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
 } from "./ui/pagination";
 
 const PageRouter = ({ page }: { page: number }) => {
   return (
     <Pagination>
       <PaginationContent>
-        {Array.from({ length: 10 }, (_, index) => (
+        <PaginationItem>
+          <PaginationPrevious
+            href={page > 1 ? `/movies?page=${page - 1}` : undefined}
+          />
+        </PaginationItem>
+        {Array.from({ length: 4 }, (_, index) => (
           <PaginationItem key={index + 1}>
             <PaginationLink href={`/movies?page=${index + 1}`}>
               {index + 1}
             </PaginationLink>
           </PaginationItem>
         ))}
+        <PaginationItem>
+          <PaginationEllipsis />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="/movies?page=50">50</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationNext
+            href={page < 50 ? `/movies?page=${page + 1}` : undefined}
+          />
+        </PaginationItem>
       </PaginationContent>
     </Pagination>
   );
