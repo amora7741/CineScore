2a458184e4f4ec18a12cac070f6fb4279c06b5a6
amora7741 <mora7741@gmail.com>
2025-01-08 13:42:30 -0800

    Setup loading/error state for movie fetching

--- src/components/Movies.tsx
+++ src/components/Movies.tsx
@@ -1,18 +1,33 @@
 import axios from "axios";
+import { LoaderCircle } from "lucide-react";
 import useSWR from "swr";
 
 const fetcher = (url: string) => axios.get(url).then((res) => res.data);
 
 const Movies = ({ page }: { page: number }) => {
-  const { data: movies, isLoading } = useSWR(
+  const { data: movies, isLoading } = useSWR<Movie[]>(
     `/api/movies?page=${page}`,
     fetcher,
   );
 
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center">
+        <LoaderCircle className="size-16 animate-spin" />
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <h1>[Movies go here]</h1>
-    </div>
+    <>
+      {movies ? (
+        <div className="grid grid-cols-3 gap-4"></div>
+      ) : (
+        <p className="self-center justify-self-center text-center text-xl font-bold sm:text-3xl">
+          There was an error fetching the movies :(
+        </p>
+      )}
+    </>
   );
 };
 
