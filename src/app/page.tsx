65f26691b0d131803f4663671a9f83688021a9b1
amora7741 <mora7741@gmail.com>
2025-01-08 13:15:48 -0800

    Placeholder info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,15 @@ import Link from "next/link";
 
 export default async function Home() {
   return (
-    <main className="my-48 flex justify-center">
+    <main className="my-60 flex justify-center">
       <div className="flex flex-col items-center gap-4">
         <h1 className="text-7xl font-bold">CineScore</h1>
-        <p>Browse through the most popular movies!</p>
+        <p className="max-w-md text-center text-lg">
+          Discover and explore popular movies, create your personal favorites
+          list, and keep track of films you love.
+        </p>
         <Button asChild>
-          <Link href="/movies">Get Started</Link>
+          <Link href="/movies">Start Browsing</Link>
         </Button>
       </div>
     </main>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     <nav className="sticky top-0 z-50 flex w-full items-center justify-between bg-background/5 p-4 shadow-sm backdrop-blur-xl">
       <Link href="/" className="flex items-center">
         <Popcorn className="size-8 -rotate-12 text-red-500" />
-        <h1 className="z-10 text-3xl font-bold">CineScore</h1>
+        <h1 className="z-10 hidden text-3xl font-bold sm:flex">CineScore</h1>
       </Link>
       <Button variant="ghost" className="text-lg">
         Sign In
