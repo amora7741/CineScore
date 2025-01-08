f048c5b1ae9985bf5571f9ed355d60f225f975ba
amora7741 <mora7741@gmail.com>
2025-01-08 14:36:16 -0800

    Display movies

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -3,7 +3,11 @@
 import Movies from "@/components/Movies";
 import PageRouter from "@/components/PageRouter";
 import { MAX_PAGES } from "@/lib/constants";
+import axios from "axios";
 import { useSearchParams } from "next/navigation";
+import useSWR from "swr";
+
+const fetcher = (url: string) => axios.get(url).then((res) => res.data);
 
 const MoviesPage = () => {
   const searchParams = useSearchParams();
@@ -12,9 +16,14 @@ const MoviesPage = () => {
     Math.min(MAX_PAGES, Number(searchParams.get("page")) || 1),
   );
 
+  const { data: movies, isLoading } = useSWR<Movie[]>(
+    `/api/movies?page=${page}`,
+    fetcher,
+  );
+
   return (
     <main className="relative grid grid-rows-[1fr_auto] gap-4 p-4">
-      <Movies page={page} />
+      <Movies movies={movies} isLoading={isLoading} />
       <PageRouter page={page} />
     </main>
   );
--- src/components/Movies.tsx
+++ src/components/Movies.tsx
@@ -1,14 +1,41 @@
-import axios from "axios";
-import { LoaderCircle } from "lucide-react";
-import useSWR from "swr";
+"use client";
 
-const fetcher = (url: string) => axios.get(url).then((res) => res.data);
+import { LoaderCircle, X } from "lucide-react";
+import { AnimatePresence, motion } from "framer-motion";
+import { useEffect, useId, useRef, useState } from "react";
+import { useOutsideClick } from "@/hooks/use-outside-click";
 
-const Movies = ({ page }: { page: number }) => {
-  const { data: movies, isLoading } = useSWR<Movie[]>(
-    `/api/movies?page=${page}`,
-    fetcher,
-  );
+import Image from "next/image";
+
+const Movies = ({
+  movies,
+  isLoading,
+}: {
+  movies: Movie[] | undefined;
+  isLoading: boolean;
+}) => {
+  const [active, setActive] = useState<Movie | boolean | null>(null);
+  const id = useId();
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    function onKeyDown(event: KeyboardEvent) {
+      if (event.key === "Escape") {
+        setActive(false);
+      }
+    }
+
+    if (active && typeof active === "object") {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+
+    window.addEventListener("keydown", onKeyDown);
+    return () => window.removeEventListener("keydown", onKeyDown);
+  }, [active]);
+
+  useOutsideClick(ref, () => setActive(null));
 
   if (isLoading) {
     return (
@@ -18,15 +45,133 @@ const Movies = ({ page }: { page: number }) => {
     );
   }
 
+  if (!movies) {
+    return (
+      <p className="self-center justify-self-center text-center text-xl font-bold sm:text-3xl">
+        There was an error fetching the movies :(
+      </p>
+    );
+  }
+
   return (
     <>
-      {movies ? (
-        <div className="grid grid-cols-3 gap-4"></div>
-      ) : (
-        <p className="self-center justify-self-center text-center text-xl font-bold sm:text-3xl">
-          There was an error fetching the movies :(
-        </p>
-      )}
+      <AnimatePresence>
+        {active && typeof active === "object" && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className="fixed inset-0 z-10 size-full bg-black/20"
+          />
+        )}
+      </AnimatePresence>
+      <AnimatePresence>
+        {active && typeof active === "object" ? (
+          <div className="fixed inset-0 z-[100] grid place-items-center">
+            <motion.button
+              key={`button-${active.title}-${id}`}
+              layout
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{
+                opacity: 0,
+                transition: { duration: 0.05 },
+              }}
+              className="absolute right-2 top-2 flex size-16 items-center justify-center rounded-full bg-white text-white lg:hidden"
+              onClick={() => setActive(null)}
+            >
+              <X />
+            </motion.button>
+
+            <motion.div
+              layoutId={`card-${active.id}-${id}`}
+              ref={ref}
+              className="flex size-full max-w-[500px] flex-col overflow-hidden bg-white dark:bg-neutral-900 sm:rounded-3xl md:h-fit md:max-h-[90%]"
+            >
+              <motion.div layoutId={`image-${active.id}-${id}`}>
+                <Image
+                  width={500}
+                  height={750}
+                  src={`https://image.tmdb.org/t/p/w500${active.poster_path}`}
+                  alt={active.title || ""}
+                  className="h-80 w-full object-cover object-top sm:rounded-t-lg lg:h-80"
+                />
+              </motion.div>
+
+              <div>
+                <div className="flex items-start justify-between p-4">
+                  <div>
+                    <motion.h3
+                      layoutId={`title-${active.id}-${id}`}
+                      className="text-base font-medium text-neutral-700 dark:text-neutral-200"
+                    >
+                      {active.title}
+                    </motion.h3>
+                    <motion.p
+                      layoutId={`release-${active.id}-${id}`}
+                      className="text-base text-neutral-600 dark:text-neutral-400"
+                    >
+                      {active.release_date}
+                    </motion.p>
+                  </div>
+
+                  <motion.div
+                    layout
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    className="rounded-full bg-green-500 px-4 py-3 text-sm font-bold text-white"
+                  >
+                    {active.vote_average?.toFixed(1)}
+                  </motion.div>
+                </div>
+                <div className="relative px-4 pt-4">
+                  <motion.div
+                    layout
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    className="flex h-40 flex-col items-start gap-4 overflow-auto pb-10 text-xs text-neutral-600 [-ms-overflow-style:none] [-webkit-overflow-scrolling:touch] [mask:linear-gradient(to_bottom,white,white,transparent)] [scrollbar-width:none] dark:text-neutral-400 md:h-fit md:text-sm lg:text-base"
+                  >
+                    {active.overview}
+                  </motion.div>
+                </div>
+              </div>
+            </motion.div>
+          </div>
+        ) : null}
+      </AnimatePresence>
+
+      <ul className="mx-auto grid w-full max-w-screen-2xl grid-cols-1 items-start gap-4 md:grid-cols-5">
+        {movies.map((movie) => (
+          <motion.div
+            layoutId={`card-${movie.id}-${id}`}
+            key={movie.id}
+            onClick={() => setActive(movie)}
+            className="flex cursor-pointer flex-col rounded-xl p-4 hover:bg-neutral-50 dark:hover:bg-neutral-800"
+          >
+            <div className="flex w-full flex-col gap-4">
+              <motion.div layoutId={`image-${movie.id}-${id}`}>
+                <Image
+                  width={200}
+                  height={300}
+                  src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                  alt={movie.title || ""}
+                  className="h-96 w-full rounded-lg object-cover object-top"
+                />
+              </motion.div>
+              <div className="flex flex-col items-center justify-center">
+                <motion.h3
+                  layoutId={`title-${movie.id}-${id}`}
+                  className="text-center text-base font-medium text-neutral-800 dark:text-neutral-200 md:text-left"
+                >
+                  {movie.title}
+                </motion.h3>
+              </div>
+            </div>
+          </motion.div>
+        ))}
+      </ul>
     </>
   );
 };
