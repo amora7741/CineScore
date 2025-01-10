ce3be0d8a063d3f4dfaf174e78f2f757515a05f3
amora7741 <mora7741@gmail.com>
2025-01-10 11:54:13 -0800

    Adjust search modal

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -20,12 +20,12 @@ const SearchBar = () => {
       </DialogTrigger>
 
       <DialogContent className="left-0 top-0 max-w-full translate-x-0 translate-y-0 border-none bg-transparent">
-        <div className="mx-auto my-40 flex w-full max-w-screen-xl flex-col">
+        <div className="mx-auto my-28 w-full max-w-screen-xl">
           <Input
             value={movieQuery}
             onChange={(e) => setMovieQuery(e.target.value)}
             placeholder="Search for a movie..."
-            className="rounded-none border-x-0 border-b-2 border-t-0 border-b-gray-500 bg-transparent py-8 text-2xl font-semibold sm:text-3xl md:text-5xl"
+            className="rounded-none border-x-0 border-b-2 border-t-0 border-b-gray-500 bg-transparent py-8 text-2xl font-semibold focus-visible:border-b-black sm:text-3xl md:text-5xl"
           />
         </div>
       </DialogContent>
