05fb39734d2b2a1ce227c0f2c5010da17cfe806d
amora7741 <mora7741@gmail.com>
2025-01-10 12:09:57 -0800

    Add search functionality

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -3,13 +3,23 @@
 import { useState } from "react";
 import { Button } from "./ui/button";
 import { Search } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 import { Dialog, DialogTrigger, DialogContent } from "@/components/ui/dialog";
 
 import { Input } from "@/components/ui/input";
 
 const SearchBar = () => {
+  const router = useRouter();
   const [movieQuery, setMovieQuery] = useState<string>("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!movieQuery.trim()) return;
+
+    const encodedQuery = encodeURIComponent(movieQuery.trim());
+    router.push(`/search?q=${encodedQuery}`);
+  };
 
   return (
     <Dialog>
@@ -21,12 +31,24 @@ const SearchBar = () => {
 
       <DialogContent className="left-0 top-0 max-w-full translate-x-0 translate-y-0 border-none bg-transparent">
         <div className="mx-auto my-28 w-full max-w-screen-xl">
-          <Input
-            value={movieQuery}
-            onChange={(e) => setMovieQuery(e.target.value)}
-            placeholder="Search for a movie..."
-            className="rounded-none border-x-0 border-b-2 border-t-0 border-b-gray-500 bg-transparent py-8 text-2xl font-semibold focus-visible:border-b-black sm:text-3xl md:text-5xl"
-          />
+          <form onSubmit={handleSubmit}>
+            <div className="relative">
+              <Input
+                type="search"
+                value={movieQuery}
+                onChange={(e) => setMovieQuery(e.target.value)}
+                placeholder="Search for a movie..."
+                className="rounded-none border-x-0 border-b-2 border-t-0 border-b-gray-500 bg-transparent py-8 text-2xl font-semibold focus-visible:border-b-black sm:text-3xl md:text-5xl"
+              />
+              <Button
+                className="absolute right-0 top-1/4"
+                variant="ghost"
+                type="submit"
+              >
+                <Search className="!size-6 stroke-[3]" />
+              </Button>
+            </div>
+          </form>
         </div>
       </DialogContent>
     </Dialog>
