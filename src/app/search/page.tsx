6f1f6f70035a03efe0d0997eeb366800ca4ef8bd
amora7741 <mora7741@gmail.com>
2025-01-10 16:31:19 -0800

    Set up search page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,7 +1,17 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+
 const SearchPage = () => {
+  const searchParams = useSearchParams();
+
+  const movieQuery = decodeURIComponent(searchParams.get("q")!);
+
   return (
-    <main className="p-8">
-      <h1>Search Page</h1>
+    <main className="relative grid grid-rows-[auto_1fr_auto] gap-8 p-8">
+      <p className="line-clamp-1 text-xl">
+        Search results for: <span className="font-bold">{movieQuery}</span>
+      </p>
     </main>
   );
 };
