073b4d186519ac2962402c0728dcbd29043cdd77
amora7741 <mora7741@gmail.com>
2025-01-13 17:34:13 -0800

    Add hero image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,27 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import Image from "next/image";
 
 export default async function Home() {
   return (
-    <main className="my-60 flex justify-center">
-      <div className="flex flex-col items-center gap-4">
-        <h1 className="text-7xl font-bold">CineScore</h1>
-        <p className="max-w-md text-center text-lg">
-          Discover and explore popular movies, create your personal favorites
-          list, and keep track of films you love.
-        </p>
-        <Button asChild>
-          <Link href="/movies">Start Browsing</Link>
-        </Button>
+    <main className="relative overflow-hidden">
+      <Image
+        src="/movies.png"
+        alt="Background image"
+        fill
+        className="absolute -z-50 scale-105 object-cover"
+      />
+      <div className="grid size-full place-items-center bg-background/80 backdrop-blur-lg">
+        <div className="flex flex-col items-center gap-4">
+          <h1 className="text-7xl font-bold">CineScore</h1>
+          <p className="max-w-md text-center text-lg">
+            Discover and explore popular movies, create your personal favorites
+            list, and keep track of films you love.
+          </p>
+          <Button asChild>
+            <Link href="/movies">Start Browsing</Link>
+          </Button>
+        </div>
       </div>
     </main>
   );
