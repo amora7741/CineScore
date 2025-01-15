812d1c8322b9728d026520c8d3d06e5f3fa8cccc
amora7741 <mora7741@gmail.com>
2025-01-14 17:47:52 -0800

    Setup dropdown menu

--- src/components/UserButton.tsx
+++ src/components/UserButton.tsx
@@ -1,6 +1,16 @@
 import { getServerAuthSession } from "@/lib/auth";
 import { Button } from "./ui/button";
 import Link from "next/link";
+import { User } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
 
 const UserButton = async () => {
   const session = await getServerAuthSession();
@@ -8,10 +18,31 @@ const UserButton = async () => {
   return (
     <>
       {session ? (
-        <h1>User: ${session.user.username}</h1>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost">
+              <span className="hidden text-lg sm:flex">
+                {session.user.username}
+              </span>
+              <User className="!size-6 stroke-[3] sm:hidden" />
+            </Button>
+          </DropdownMenuTrigger>
+
+          <DropdownMenuContent>
+            <DropdownMenuLabel>{session.user.username}</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+
+            <DropdownMenuGroup>
+              <DropdownMenuItem></DropdownMenuItem>
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
       ) : (
         <Button variant="ghost" className="text-lg">
-          <Link href="/login">Sign In</Link>
+          <Link href="/login">
+            <span className="hidden sm:flex">Sign In</span>
+            <User className="!size-6 stroke-[3] sm:hidden" />
+          </Link>
         </Button>
       )}
     </>
