ad8437acbfd14da993fe465a88090cd620985355
amora7741 <mora7741@gmail.com>
2025-01-17 13:23:28 -0800

    Add signout functionality

--- src/components/SignOut.tsx
+++ src/components/SignOut.tsx
@@ -8,21 +8,40 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 
+import { toast } from "@/hooks/use-toast";
+import { signOut } from "next-auth/react";
+
 const SignOut = () => {
+  const handleSignOut = async () => {
+    try {
+      await signOut();
+    } catch (error) {
+      console.error(error);
+
+      toast({
+        variant: "destructive",
+        title: "Error",
+        description: "An error occurred while signing out.",
+      });
+    }
+  };
+
   return (
     <Dialog>
       <DialogTrigger>Sign Out</DialogTrigger>
 
-      <DialogContent>
+      <DialogContent className="space-y-4">
         <DialogHeader>
-          <DialogTitle>Sign Out</DialogTitle>
+          <DialogTitle className="text-xl">Sign Out</DialogTitle>
 
-          <DialogDescription>
+          <DialogDescription className="text-lg">
             Are you sure you want to sign out?
           </DialogDescription>
         </DialogHeader>
 
-        <Button>Sign Out</Button>
+        <Button className="w-full sm:ml-auto sm:w-fit" onClick={handleSignOut}>
+          Sign Out
+        </Button>
       </DialogContent>
     </Dialog>
   );
